bf846353d11d3437419c542f44a903e7ecd2fd05
B-Rabbit7 <setarehnr60@gmail.com>
2022-05-08 16:15:04 -0700

    added modemon

--- Assignment 1/server.js
+++ Assignment 1/server.js
@@ -1,7 +1,28 @@
 const express = require('express')
 const app = express()
+app.set('view engine', 'ejs');
+
 
 app.listen(5000, function (err) {
     if (err)
         console.log(err);
 })  
+
+// app.get('/', function (req, res) {
+//     res.send('<h1> GET request to homepage</h1>')
+// })
+
+app.get('/profile/:id', function (req, res) {
+    // req.params.id
+    console.log(req)
+    // res.write(`<h1> This pokemon has id: ${req.params.id}</h1>`)
+    // res.send();
+    res.render("profile.ejs", {
+        "id": req.params.id
+    })
+})
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + "/index.html");
+//   })
+
+app.use(express.static('./public'));
