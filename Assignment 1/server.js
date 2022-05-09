30d291f7e586f0a453ebb00592425265e185a9da
B-Rabbit7 <setarehnr60@gmail.com>
2022-05-08 18:53:02 -0700

    finished profile function

--- Assignment 1/server.js
+++ Assignment 1/server.js
@@ -2,25 +2,49 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'ejs');
 
-
 app.listen(5000, function (err) {
     if (err)
         console.log(err);
-})  
+})
 
 // app.get('/', function (req, res) {
-//     res.send('<h1> GET request to homepage</h1>')
+//     res.send('<h1> GET request to homepage </h1>')    
 // })
 
+
+const https = require('https');
 app.get('/profile/:id', function (req, res) {
-    // req.params.id
-    console.log(req)
-    // res.write(`<h1> This pokemon has id: ${req.params.id}</h1>`)
-    // res.send();
-    res.render("profile.ejs", {
-        "id": req.params.id
-    })
+    
+    const url = `https://pokeapi.co/api/v2/pokemon/${req.params.id}`
+    data = ""
+    https.get(url, function (https_res) {
+        https_res.on("data", function (chunk) {
+            data += chunk
+        })
+        
+        https_res.on("end", function () {
+            data = JSON.parse(data)
+            
+            hpPoke = data.stats.filter((obj_)=>{
+                return obj_.stat.name == "hp"
+            }).map((obj2)=>{
+                return obj2.base_stat
+            })
+            res.render("profile.ejs", {
+                "id": req.params.id,
+                "name": data.name,
+                "hp": hpPoke[0],
+                "imagePoke": data.sprites.other["official-artwork"]["front_default"],
+                "type": data.types[0].type.name,
+                "weight": data.weight
+            });
+        })
+    });
+   
+    console.log(data);
+   
 })
+
 // app.get('/', function(req, res) {
 //     res.sendFile(__dirname + "/index.html");
 //   })
