4e4beebb79d6e1ecc08718af6800fdaf7767a43c
B-Rabbit7 <setarehnr60@gmail.com>
2022-05-12 14:04:20 -0700

    hit problem with history

--- Assignment 1/public/search.js
+++ Assignment 1/public/search.js
@@ -1,13 +1,20 @@
-typePoke = " ";
+typePoke = "normal";
+showPoke = '';
 abilityPoke = " " ;
+ability = '';
+namePoke = '';
+
+// Type
 function processByType(data){
+    showPoke = ''
     for (i = 0 ; i < data.types.length; i++)
         if(data.types[i].type.name == typePoke)
-            $("main").append(`<div class = "PokeType"><a href="/profile/${data.id}"><img src="${data.sprites.front_default}" alt="${data.name}"></a></div>`)
+            showPoke += `<div class = "PokeType"><a href="/profile/${data.id}"><img src="${data.sprites.front_default}" alt="${data.name}"></a></div>`
+    
+    $("main").append(showPoke)
 }
 
 function displayPokeType(by_type){
-    $("main").empty();
     typePoke = by_type;
     for(i=1; i<1126; i++) {
         $.ajax({
@@ -18,8 +25,11 @@ function displayPokeType(by_type){
     }
 
 }
+
+// Name
 function showPoke_name(data){
     console.log(data);
+    namePoke  = data.name
     reveal_pokemon = '';
     reveal_pokemon += `<div class="img-container" id="pokeReveal"> <b>This Pokemon Name:</b> ${data.name} <br>
     <a href="/profile/${data.id}">
@@ -27,20 +37,10 @@ function showPoke_name(data){
     </a>
     <br>Click on the Pokemon to reveal it's stats!</div>`
     $("main").html(reveal_pokemon);
+    poke_button = `<button id ="${data.name}" onclick="history_return(reveal_pokemon)">${data.name}</button>`
+    append_history();
 }
 
-function showPoke_ability(data){
-    console.log(data)
-    console.log(data.name)
-    console.log(data.pokemon)
-    console.log(data.pokemon.length) //for loop here
-    show_pokemon = '';
-    
-    // for(i=0; i<data.pokemon.length; i++){
-    //     show_pokemon +=
-    // }
-    
-}
 
 function alertError(){
     alert("Wrong Input!")
@@ -56,22 +56,89 @@ function searchPokeName(){
         error: alertError
     })
 }
+
+// get Pokemon Ability 
+
+function get_pokemon_ability(data){
+    console.log(data)
+    ability += `  <div class="img-container"> ${data.name} <br>
+    <a href="/profile/${data.id}">
+    <img src="${data.sprites.other["official-artwork"].front_default}"> 
+    </a>
+    <br>
+    ${data.types[0].type.name} type
+    </div>`
+
+}
 function searchPokeAbility(){
+
     console.log("This button was pressed")
     var searchAbility = $("#ability_input").val();
     abilityPoke = searchAbility
+    console.log(abilityPoke);
     $.ajax({
         "url": `https://pokeapi.co/api/v2/ability/${abilityPoke}`,
         "type": "GET",
         "success": showPoke_ability,
         error: alertError
     })
-    
-
-    // console.log(typeof(searchHeight) )
-
-
 }
+async function showPoke_ability(data) {
+    ability = '';
+    console.log("function called")
+    console.log(abilityPoke);
+    console.log(data)
+    console.log(data.pokemon)
+    console.log(data.pokemon[0])
+    console.log(data.pokemon[0].pokemon)
+    // get to pokemon name
+    console.log(data.pokemon[0].pokemon.name)
+    for(i = 1; i < data.pokemon.length; i ++){
+        if (i % 3 == 1) { 
+            ability += `<div class="clearfix">`
+        }
+
+        await $.ajax({
+            "url": `https://pokeapi.co/api/v2/pokemon/${data.pokemon[i].pokemon.name}/`,
+            "type": "GET",
+            "success": get_pokemon_ability
+        })
+
+
+        if (i % 3 == 0) { // only when i= 3, 6, 9
+            ability += `</div>`
+        }
+    }
+    $("main").html(ability)
+    poke_button = `<button id= "${data.pokemon[0].pokemon.name}" onclick="history_return(ability)">${abilityPoke}</button>`
+    append_history();
+}
+
+// History Section
+
+function append_history(){
+    button_text = "<button class='remove_button'>Hide this</button>"
+    styled_output = "<span id='search'>" + poke_button + button_text +"</span>";
+    $('.history').append(styled_output)
+}
+
+function history_return(data){
+    $("#name_input").empty();
+    $("#ability_input").empty();
+    $("main").html(data)
+}
+
+// History buttons
+function hide_(){
+    $(this).parent().remove();
+}
+
+function clear(){
+    $('.history').html(" ")
+}
+
+//set up function
+
 function setup(){
     displayPokeType($("#poke_type option:selected").val())
     $("#poke_type").change(() => {
@@ -80,6 +147,9 @@ function setup(){
     })
     $("#name").click(searchPokeName)
     $("#ability").click(searchPokeAbility)
+    $("#clear").click(clear)
+
+    $('body').on("click", ".remove_button", hide_);
 
 }
 $(document).ready(setup)
